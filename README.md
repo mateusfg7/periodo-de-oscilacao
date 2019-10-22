ec89004ac810029b3de8fd28ce8a7e3009684492
mateusfg7 <mateusfg77@gmail.com>
2019-10-22 17:42:26 -0300

    Adicionado Novos Astros

--- README.md
+++ README.md
@@ -8,20 +8,61 @@ Calcula o perildo de ocilação de um pêndulo
 ### L - Comprimento da corda
 
 ### g - Aceleração Gravitacional 
-- Terra: g = 9,8 m/s²
+- Sol:      g = 274,13 m/s²
+- Mercúrio: g = 3,78 m/s²
+- Venus:    g = 8,60 m/s²
+- Terra:    g = 9,8 m/s²
+- Marte:    g = 3,72 m/s²
+- Júpiter:  g = 24,8 m/s²
+- Saturno:  g = 10,5 m/s²
+- Urano:    g = 8,5 m/s²
+- Netuno:   g = 10,8 m/s²
+- Plutão:   g = 5,88 m/s²
+- Lua:      g = 1,67 m/s²
 ---
 
 ## Use:
 ```
-ocilacao.py [calculo] [base]
+ocilacao.py [calculo] [astro] [base]
 ```
 ### Calculo
 > t - tempo de ocilação
 
 > c - comprimento do pêndulo
 
+### Astro
+> sol - Sol
+
+> mer - Mercúrio
+
+> ven - Venus
+
+> ter - Terra
+
+> mar - Marte
+
+> jup - Júpiter
+
+> sat - Saturno
+
+> ura - Urano
+
+> net - Netuno
+
+> plu - Plutão
+
+> lua - Lua
+
 ### Base
 > Comprimento da corda em centímetros para retornar o tempo de ocilação.
 
 > Tempo de ocilação em segundos para retornar o comprimento da corda.
+
+## Ex:
+```
+ocilacao.py t ter 50
+```
+Calcula o tempo de ocilação (em segundos) na terra com base no comprimento da corda (em centímetros)
+
+![preview](preview.png)
 
--- ocilacao.py
+++ ocilacao.py
@@ -1,33 +1,73 @@
 from math import pi, sqrt
 from sys import argv as param
 
-def Tempo(comprimento):
-    comprimentoEmCm = comprimento
-    L = comprimentoEmCm/100
-    g = 9.8
-    T = (2 * pi) * sqrt(L / g)
-    print("T ≅ {:.2f}s".format(T))
-
-def Comprimento(tempo):
-    g = 9.8
-    T = tempo
-    L = (T/4*(pi**2))*g
-    print("L ≅ {:.2f}cm".format(L))
 
 try:
     parametroUm = param[1]
-    
-    try:
-        parametroDois = float(param[2])
-    except ValueError:
-        print("Valor inválido ou inexistente!")
-        exit()
-
+    parametroDois = param[2]
+    parametroTres = float(param[3])
 except IndexError:
     print("Número de argumentos insuficientes!")
     exit()
+except ValueError:
+        print("Valor inválido ou inexistente!")
+        exit()
+
+
+def Tempo(comprimento, gravidade):
+    comprimentoEmCm = comprimento
+    L = comprimentoEmCm/100
+    g = gravidade
+    T = (2 * pi) * sqrt(L / g)
+    print("T ≅ {:.5f}s".format(T))
+
+def Comprimento(tempo, gravidade):
+    g = gravidade
+    T = tempo
+    L = (T/4*(pi**2))*g
+    print("L ≅ {:.5f}cm".format(L))
+
+def Astro():
+    astros = {
+        "sol": 274.13,
+        "mercurio": 3.78,
+        "venus": 8.60,
+        "terra": 9.8,
+        "marte": 3.72,
+        "jupiter": 24.8,
+        "saturno": 10.5,
+        "urano": 8.5,
+        "netuno": 10.8,
+        "plutao": 5.88,
+        "lua": 1.67
+    }
+
+    if parametroDois == 'sol':
+        return astros['sol']
+    elif parametroDois == 'mer':
+        return astros['mercurio']
+    elif parametroDois == 'ven':
+        return astros['venus']
+    elif parametroDois == 'ter':
+        return astros['terra']
+    elif parametroDois == 'mar':
+        return astros['marte']
+    elif parametroDois == 'jup':
+        return astros['jupiter']
+    elif parametroDois == 'sat':
+        return astros['saturno']
+    elif parametroDois == 'ura':
+        return astros['urano']
+    elif parametroDois == 'net':
+        return astros['netuno']
+    elif parametroDois == 'plu':
+        return astros['plutao']
+    elif parametroDois == 'lua':
+        return astros['lua']
+
+
 
 if parametroUm == "t":
-    Tempo(parametroDois)
+    Tempo(parametroTres, Astro())
 elif parametroUm == "c":
-    Comprimento(parametroDois)
+    Comprimento(parametroTres, Astro())
