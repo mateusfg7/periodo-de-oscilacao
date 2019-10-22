884ea7c6f29111d0c77291fdbcac3f7dd82639d7
mateusfg7 <mateusfg77@gmail.com>
2019-10-22 18:02:07 -0300

    add Menu

--- ocilacao.py
+++ ocilacao.py
@@ -2,15 +2,51 @@ from math import pi, sqrt
 from sys import argv as param
 
 
+def Menu():
+    print("\nUse: ocilacao.py [calculo] [astro] [base]")
+    
+    print("\nCalculo")
+    print("""
+    t   tempo de ocilação
+    c   comprimento do pêndulo
+    """)
+    
+    print("\nAstro")
+    print("""
+    sol   Sol
+    mer   Mercúrio
+    ven   Venus
+    ter   Terra
+    mar   Marte
+    jup   Júpiter
+    sat   Saturno
+    ura   Urano
+    net   Netuno
+    plu   Plutão
+    lua   Lua
+    """)
+
+    print("\nBase")
+    print("""
+    Comprimento da corda em centímetros para retornar o tempo de ocilação.
+    Tempo de ocilação em segundos para retornar o comprimento da corda.
+    """)
+
+    print("\nhelp   retornar esse menu")
+
+
 try:
     parametroUm = param[1]
-    parametroDois = param[2]
-    parametroTres = float(param[3])
+    if parametroUm != 'help':
+        parametroDois = param[2]
+        parametroTres = float(param[3])
 except IndexError:
     print("Número de argumentos insuficientes!")
+    Menu()
     exit()
 except ValueError:
         print("Valor inválido ou inexistente!")
+        Menu()
         exit()
 
 
@@ -67,7 +103,11 @@ def Astro():
 
 
 
+
+
 if parametroUm == "t":
     Tempo(parametroTres, Astro())
 elif parametroUm == "c":
     Comprimento(parametroTres, Astro())
+elif parametroUm == "help":
+    Menu()
